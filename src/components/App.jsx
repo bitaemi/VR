17359223db38af272e0d97856739fbfa699458a5
Kamal Farsaoui <elfa.kamal@gmail.com>
2017-08-16 17:36:20 +0200

    refactor: a little refactoring of assets and components

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import logo from '../assets/logo.svg';
+import '../assets/styles/app.css';
+
+import Header from './Header';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
